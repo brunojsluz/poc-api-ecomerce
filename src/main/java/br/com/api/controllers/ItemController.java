ca880b129e27aad8bc6add5828dce4639b7b1f68
Bruno Luz <brunojsluz@gmail.com>
2017-10-26 20:28:45 -0200

    Ajuste para aceitar string vazia

--- src/main/java/br/com/api/controllers/ItemController.java
+++ src/main/java/br/com/api/controllers/ItemController.java
@@ -3,15 +3,7 @@ package br.com.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.api.dto.AlteraPrecoDTO;
 import br.com.api.dto.ItemDTO;
@@ -34,8 +26,8 @@ public class ItemController {
 		return service.buscarItem(codigo);
 	}
 	
-	@GetMapping("/busca/parcial/{descricao}")
-	public List<ItemDTO> buscarPorDescricao(@PathVariable("descricao") String descricao) {
+	@GetMapping("/busca/parcial")
+	public List<ItemDTO> buscarPorDescricao(@RequestParam("descricao") String descricao) {
 		return service.buscarPorDescricao(descricao);
 	}
 	
